bb5f29c61ee7d56cff20f7b9d7903f866090cf25
EllaEnzo <109322815+EllaEnzo@users.noreply.github.com>
2022-11-12 03:15:31 -1200

    Un cambio

--- src/probar/App.java
+++ src/probar/App.java
@@ -4,7 +4,7 @@ public class App {
     public static void main(String[] args){
         System.out.println("OK!");
         System.out.println("PP");
-        System.out.println("lola");
+        System.out.println("lo");
     }
 
 }
